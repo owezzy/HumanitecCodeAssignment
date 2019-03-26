ab644c393e072c4751a78004df4621e25e7f2338
owezzy <owenobezzy@gmail.com>
2019-03-26 12:44:23 +0300

    purge repository to favor nx-workspace workflow

--- src/app/programs/activity.ts
+++
@@ -1,6 +0,0 @@
-export interface Activity {
-  id: number | null;
-  name: string;
-  create_date: string;
-  end_date: string;
-}
--- src/app/programs/program.ts
+++
@@ -1,6 +0,0 @@
-export interface Program {
-  id: number | null;
-  name: string;
-  level1_uuid: string;
-}
-
