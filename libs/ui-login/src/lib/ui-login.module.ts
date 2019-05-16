fa957308e600af5bbb80c32633575852f8d13d6c
owezzy <owenobezzy@gmail.com>
2019-05-16 08:46:26 +0300

    ui-login component library

---
+++ libs/ui-login/src/lib/ui-login.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from '@humanitec/ui-login';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LoginComponent]
+})
+export class UiLoginModule {}
