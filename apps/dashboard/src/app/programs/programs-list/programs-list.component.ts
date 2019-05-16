0c435fd3111c35479a211e4fb4386c37833e75db
owezzy <owenobezzy@gmail.com>
2019-05-16 15:49:27 +0300

    define programs list view and component details

--- apps/dashboard/src/app/programs/programs-list/programs-list.component.ts
+++ apps/dashboard/src/app/programs/programs-list/programs-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { LIST_ANIMATION } from './programs-list.animation';
+import { ProgramModel } from '@humanitec/programs';
 
 @Component({
   selector: 'humanitec-programs-list',
   templateUrl: './programs-list.component.html',
-  styleUrls: ['./programs-list.component.scss']
+  styleUrls: ['./programs-list.component.scss'],
+  animations: [LIST_ANIMATION]
 })
 export class ProgramsListComponent implements OnInit {
+  @Input() programs: ProgramModel[];
+  @Input() readonly = false;
+  @Output() selected = new EventEmitter();
+  @Output() deleted = new EventEmitter();
 
-  constructor() { }
+  animationsDisabled = true;
+
+  trackProgram(index, program) {
+    return program.id;
+  }
 
   ngOnInit() {
+    setTimeout(()=> {
+      this.animationsDisabled = false;
+    }, 500)
+  }
+
+  prepareListState () {
+    return this.programs ? this.programs.length : 'pending';
   }
 
 }
