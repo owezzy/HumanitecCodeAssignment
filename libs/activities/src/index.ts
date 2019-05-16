b9dedb4f376f21a7a8ae08c1ab9eed296ebfd4ca
owezzy <owenobezzy@gmail.com>
2019-05-16 15:47:19 +0300

    export modules and components

--- libs/activities/src/index.ts
+++ libs/activities/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/+state/activities.reducer';
 export * from './lib/+state/activities.selectors';
 export * from './lib/+state/activities.effects'
 export * from './lib/activities.module';
+export {ActivityModel} from './lib/activity-model'
+export {ActivityService} from './lib/activity-service.service'
--- libs/ui-login/src/index.ts
+++ libs/ui-login/src/index.ts
@@ -1,2 +1,2 @@
-export { UiLoginModule } from './lib/ui-login.module';
+export  *  from './lib/ui-login.module';
 export { LoginComponent } from './lib/login/login.component';
