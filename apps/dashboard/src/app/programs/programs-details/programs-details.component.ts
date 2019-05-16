66e9807dd096f77ef08251f11af514c729d2760b
owezzy <owenobezzy@gmail.com>
2019-05-16 15:50:07 +0300

    define program detail view and component details

--- apps/dashboard/src/app/programs/programs-details/programs-details.component.ts
+++ apps/dashboard/src/app/programs/programs-details/programs-details.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ProgramModel } from '@humanitec/programs';
+import { ActivityModel } from '@humanitec/activities';
 
 @Component({
   selector: 'humanitec-programs-details',
   templateUrl: './programs-details.component.html',
   styleUrls: ['./programs-details.component.scss']
 })
-export class ProgramsDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ProgramsDetailsComponent {
+  originalTitle: string;
+  selectedProgram: ProgramModel;
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
+  @Input() activities: ActivityModel[];
+  @Input() set program(value: ProgramModel) {
+    if (value) {this.originalTitle = value.title;}
+    this.selectedProgram = Object.assign({}, value)
   }
-
 }
+
+
