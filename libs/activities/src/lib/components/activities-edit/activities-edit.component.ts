480ecd492931617b1368e19de2dc8870f4d6ac7f
owezzy <owenobezzy@gmail.com>
2019-05-22 12:46:42 +0300

    create view components for activities resource

---
+++ libs/activities/src/lib/components/activities-edit/activities-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'humanitec-activities-edit',
+  templateUrl: './activities-edit.component.html',
+  styleUrls: ['./activities-edit.component.scss']
+})
+export class ActivitiesEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
